4c610620f2288ed74209667dcfd607d583eebee9
VBlazing <18392192892@163.com>
2025-11-26 21:33:34 +0800

    Initial Commit

---
+++ brush_translate/brush_translateApp.swift
@@ -0,0 +1,17 @@
+//
+//  brush_translateApp.swift
+//  brush_translate
+//
+//  Created by 赵泽宇 on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct brush_translateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
